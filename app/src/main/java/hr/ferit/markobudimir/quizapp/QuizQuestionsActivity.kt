badcc91002a08638eda87331f0739ee2f1f50a3f
Marko Budimir <marko.budimir12@gmail.com>
2022-03-02 22:17:43 +0100

    Add btnSubmit

--- app/src/main/java/hr/ferit/markobudimir/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/hr/ferit/markobudimir/quizapp/QuizQuestionsActivity.kt
@@ -45,6 +45,7 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         tvOptionTwo?.setOnClickListener(this)
         tvOptionThree?.setOnClickListener(this)
         tvOptionFour?.setOnClickListener(this)
+        btnSubmit?.setOnClickListener(this)
 
         questionsList = Constants.getQuestions()
 
@@ -62,6 +63,7 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         tvOptionTwo?.text = question.optionTwo
         tvOptionThree?.text = question.optionThree
         tvOptionFour?.text = question.optionFour
+
 
         if(currentPosition == questionsList!!.size){
             btnSubmit?.text = "FINISH"
@@ -127,6 +129,55 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
                 tvOptionFour?.let{
                     selectedOptionView(it, 4)
                 }
+            }
+            R.id.btnSubmit ->{
+                if(selectedOptionPosition == 0){
+                    currentPosition++
+                    when{
+                        currentPosition <= questionsList!!.size ->{
+                            setQuestion()
+                        }
+                    }
+                }else{
+                    val question = questionsList?.get(currentPosition-1)
+                    if(question!!.correctAnswer != selectedOptionPosition){
+                        answerView(selectedOptionPosition, R.drawable.wrong_option_border_bg)
+                    }
+                    answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
+
+                    if(currentPosition == questionsList!!.size){
+                        btnSubmit?.text = "FINISH"
+                    }else{
+                        btnSubmit?.text = "GO TO NEXT QUESTION"
+                    }
+
+                    selectedOptionPosition = 0
+                }
+            }
+        }
+    }
+
+    private fun answerView(answer: Int, drawableView: Int){
+        when(answer){
+            1 -> {
+                tvOptionOne?.background = ContextCompat.getDrawable(
+                    this, drawableView
+                )
+            }
+            2 -> {
+                tvOptionTwo?.background = ContextCompat.getDrawable(
+                    this, drawableView
+                )
+            }
+            3 -> {
+                tvOptionThree?.background = ContextCompat.getDrawable(
+                    this, drawableView
+                )
+            }
+            4 -> {
+                tvOptionFour?.background = ContextCompat.getDrawable(
+                    this, drawableView
+                )
             }
         }
     }
